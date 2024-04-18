bfbf949819cfcc6b53212c0b9b4e5655ebc17333
ribkafuguestmarcobky <167439282+ribkafuguestmarcobky@users.noreply.github.com>
2024-04-18 16:30:54 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+const time = document.getElementById("time")
+const day = document.getElementById("day")
+
+let clock = setInterval(
+    function calcTime() {
+        let date_now = new Date();
+        let hr = date_now.getHours();
+        let min = date_now.getMinutes();
+        let sec = date_now.getSeconds();
+
+        let days = [
+            "Воскресение",
+            "Понедельник",
+            "Вторник",
+            "Среда",
+            "Четверг",
+            "Пятница",
+            "Суббота"
+        ];
+        day.textContent = days[date_now.getDay()];
+
+        hr = hr < 10 ? "0" + hr : hr;
+        min = min < 10 ? "0" + min : min;
+        sec = sec < 10 ? "0" + sec : sec;
+
+        time.textContent = hr + ":" + min + ":" + sec
+    
+
+    },
+);
